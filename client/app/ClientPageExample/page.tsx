2f4e78b7119db79587c392d230c724de8fdad063
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-03-15 20:14:14 -0400

    .

---
+++ client/app/ClientPageExample/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+const PageOneExample = () => {
+  return (
+    <div>
+      <h1>This is the /ClientPageExample route</h1>
+      <h1>
+        This is a client rendered page, because it does have 'use client' at the top
+        of this file
+      </h1>
+    </div>
+  )
+}
+
+export default PageOneExample
---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from '@styles/App.module.scss'
+
+export default function Index() {
+  return (
+    <div className={styles.test}>
+      <h1>This is the / route</h1>
+    </div>
+  )
+}
