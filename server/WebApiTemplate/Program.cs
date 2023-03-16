374db115850faaa08bbff71810e527497edd9245
Gary Sharp <7181962+mlancer@users.noreply.github.com>
2023-03-15 22:17:44 -0400

    -Added super basic implementation of SignalR following Microsoft Tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/signalr-typescript-webpack?view=aspnetcore-7.0&tabs=visual-studio.
    -Added Razor pages for testing but could probably leave this for now while additional fields are needed as well as familiarization of SignalR.
    
    -Added HackWeekly MySQL connection string to appsettings. If MySQL is the route we are taking then everyones local will need to be configured.

--- server/WebApiTemplate/Program.cs
+++ server/WebApiTemplate/Program.cs
@@ -5,6 +5,7 @@ using EmeraldChameleonChat.DAL.DbContexts;
 using EmeraldChameleonChat.DAL.Repository;
 using EmeraldChameleonChat.DAL.Repository.RepositoryInterfaces;
 using EmeraldChameleonChat.Model.Entity;
+using EmeraldChameleonChat.Hubs;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
@@ -17,6 +18,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 
 builder.Services.AddAutoMapper(x =>
 {
@@ -28,9 +30,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<EmeraldChameleonChatContext>(
     dbContextOptions => dbContextOptions.UseSqlite("Data Source=WeatherInfo.db"));// adds the dbcontext with a scoped lifetime
 builder.Services.AddScoped<IWeatherForecastRepository, WeatherForecastRepository>();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -38,8 +46,14 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseCors(x => x
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            //.WithOrigins("https://localhost:44351")); // Allow only this origin can also have multiple origins seperated with comma
+            .SetIsOriginAllowed(origin => true));// Allow any origin 
 
 app.UseRouting();
 
@@ -49,5 +63,8 @@ app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
