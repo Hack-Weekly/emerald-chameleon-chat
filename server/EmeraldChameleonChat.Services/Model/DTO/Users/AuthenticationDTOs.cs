281455a671a916e8997bdd97fd0f4082905e4c47
iMuhammadMustafa <iMuhammadMustafa@gmail.com>
2023-03-16 15:48:41 +0200

    Add Authentication Boilerplate

---
+++ server/EmeraldChameleonChat.Services/Model/DTO/Users/AuthenticationDTOs.cs
@@ -0,0 +1,53 @@
+﻿using EmeraldChameleonChat.Services.Model.Entity.Users;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmeraldChameleonChat.Services.Model.DTO.Users
+{
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        [EmailAddress, Required] public string Email { get; set; }
+        public string Mobile { get; set; }
+
+        [Required] public string Username { get; set; }
+
+        [Required] public string Password { get; set; }
+    }
+
+
+
+    public class LoginModel
+    {
+        public string? UserId { get; set; }
+        public string? Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponseModel
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+
+        public LoginResponseModel()
+        {
+
+        }
+        public LoginResponseModel(string accessToken, string refreshToken)
+        {
+            AccessToken = accessToken;
+            RefreshToken = refreshToken;
+        }
+    }
+    public class TokenValidationResponse
+    {
+        public int Id { get; set; }
+
+        public User? User { get; set; }
+
+        public TokenValidationResponse(User user)
+        {
+            User = user;
+        }
+    }
+}
---
+++ server/EmeraldChameleonChat.Services/Services/Users/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using EmeraldChameleonChat.Services.Model.DTO.Users;
+using EmeraldChameleonChat.Services.Model.Entity.Users;
+
+namespace EmeraldChameleonChat.Services.Services.Users
+{
+    public interface IAuthenticationService
+    {
+        Task<LoginResponseModel> GenerateJWToken(User registeredUser, CancellationToken cancellationToken, bool refreshExpired = false);
+        Task<LoginResponseModel> ValidateRefreshToken(string accessToken, string refreshToken, CancellationToken cancellationToken);
+
+    }
+}
