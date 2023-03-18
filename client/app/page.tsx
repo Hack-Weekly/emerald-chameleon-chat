feba44e544a8475233443ec7b8e0f306a2dd7913
Jeni <ziahrahl@gmail.com>
2023-03-18 10:43:37 -0400

    make start swiper and links their own component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,29 +1,15 @@
 import ChameleonGraphic from '@components/ChameleonGraphic'
 import IntroText from '@components/IntroText'
+import StartSwiperAndLinks from '@components/StartSwiperAndLinks'
 import styles from '@styles/App.module.scss'
-import Image from 'next/image'
+
 
 export default function Index() {
   return (
     <div className={styles.homeLayout}>
       <ChameleonGraphic />
       <IntroText />
-      <div className={styles.loginContainer}>
-        <div className={styles.slider}>
-          <span>Start</span>
-          <Image
-            priority
-            src="/images/right-arrow.svg"
-            alt="arrow"
-            width={50}
-            height={50}
-          />
-        </div>
-        <div className={styles.links}>
-          <p>Log In</p>
-          <p>Create Account</p>
-        </div>
-      </div>
+      <StartSwiperAndLinks />
     </div>
   )
 }
