70e232e37fd51e96069c0c1a2561cd78115a61d7
Damanpreet Singh <daman0797@gmail.com>
2023-03-17 21:54:57 -0400

    login page without authentication

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,15 @@
+import LoginForm from 'app/LoginPage/LoginForm'
 import styles from '@styles/App.module.scss'
 
 export default function Index() {
   return (
-    <div className={styles.header}>
-      <h1 className={styles.h1}> This is the / route</h1>
+    <div>
+      <div className={styles.header}>
+        <h1 className={styles.h1}> This is the / route</h1>
+      </div>
+      <div>
+        <LoginForm />
+      </div>
     </div>
   )
 }
