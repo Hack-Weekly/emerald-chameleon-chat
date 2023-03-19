47a8c03b44cf9631e8dc3666fe1276da8093278f
Damanpreet Singh <daman0797@gmail.com>
2023-03-18 22:49:20 -0400

    update with main

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,6 @@ import IntroText from '@components/IntroText'
 import StartSwiperAndLinks from '@components/StartSwiperAndLinks'
 import styles from '@styles/App.module.scss'
 
-
 export default function Index() {
   return (
     <div className={styles.homeLayout}>
