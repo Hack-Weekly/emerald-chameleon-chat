77afbfda47afebe151e1ed8cec3e64244428b1fd
Jeni <ziahrahl@gmail.com>
2023-03-22 14:02:10 -0400

    add conditionally rendering functionality

--- client/components/EmailConfirmation/EmailConfirmation.tsx
+++ client/components/EmailConfirmation/EmailConfirmation.tsx
@@ -3,36 +3,55 @@
 import React, { useState } from 'react'
 import ChameleonGraphic from '@components/ChameleonGraphic'
 import styles from './EmailConfirmation.module.scss'
-
+import Link from 'next/link'
 
 function EmailConfirmation() {
   console.log('Email Confirmed rendered')
   const [showForm, setShowForm] = useState(true)
+ 
+  const [responseMessage, setResponseMessage] = useState('')
   const [showSuccessOrFailureMessage, setShowSuccessOrFailureMessage] = useState(false)
 
-  let isConfirmationSuccessful = true
 
-
-  const SuccessOrFailureHandler = (isConfirmationSuccessful: boolean) => {
+  function SuccessOrFailureHandler() {
     let message = ''
     let buttonText = ''
-    if (isConfirmationSuccessful) {
+    let hrefText = ''
+    let isSuccess = false
+    if (responseMessage === 'success') {
       message = 'Yay! Email has been confirmed'
       buttonText = 'Login'
-    } else {
+      hrefText = '/Login'
+      isSuccess = true
+    } else if (responseMessage === 'failure') {
       message = 'Email could not be verified'
       buttonText = 'Try Again'
+      isSuccess = false
     }
+
+    const handleTryAgainClick = () => {
+      setShowSuccessOrFailureMessage(false)
+      setShowForm(true)
+    }
+
     return (
       <div className={styles.messageBoxWrapper}>
         <h3>{message}</h3>
-        <button>{buttonText}</button>
+        {isSuccess && <Link href={hrefText} className={styles.messageBox__link}>
+          {buttonText}
+          </Link>
+        }
+        {!isSuccess && (
+          <button className={styles.messageBox__link} onClick={handleTryAgainClick}>
+            {buttonText}
+          </button>
+        )}
       </div>
     )
   }
 
   const ConfirmationCodeForm = () => {
- 
+    const [confirmationCodeInput, setConfirmationCodeInput] = useState('')
     // const baseUrl =
     //   'https://house-plants2.p.rapidapi.com/id/53417c12-4824-5995-bce0-b81984ebbd1d'
 
@@ -46,6 +65,13 @@ function EmailConfirmation() {
     const handleSubmit = (e: { preventDefault: () => void }) => {
       e.preventDefault()
       console.log('submit button clicked')
+      if (confirmationCodeInput === 'good') {
+        setResponseMessage('success')
+      } else {
+        setResponseMessage('failure')
+      }
+      setShowForm(false)
+      setShowSuccessOrFailureMessage(true)
       // const options = {
       //   method: 'GET',
       //   headers: {
@@ -65,7 +91,13 @@ function EmailConfirmation() {
       <div>
         <form onSubmit={handleSubmit} className={styles.formWrapper}>
           <label htmlFor="confirmationCode">Confirmation code:</label>
-          <input type="text" id="confirmationCode" name="confirmationCode" />
+          <input
+            type="text"
+            id="confirmationCode"
+            name="confirmationCode"
+            value={confirmationCodeInput}
+            onChange={(e) => setConfirmationCodeInput(e.currentTarget.value)}
+          />
           <p>Please enter your code</p>
           <div className={styles.button}>
             <button className={styles.submitButton} type="submit">
