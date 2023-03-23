a38f97ebeff6110c79bbe1aa482d87db7616cdef
Jeni <ziahrahl@gmail.com>
2023-03-23 10:32:20 -0400

    move components and corresponding scss file to separate folders

--- client/components/LoginAndRegisterHandler/LoginAndRegisterHandler.tsx
+++ client/components/LoginAndRegisterHandler/LoginAndRegisterHandler.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import Image from 'next/image'
-import styles from '@styles/startSwiperAndLinks.module.scss'
+import styles from './LoginAndRegisterHandler.module.scss'
 import Link from 'next/link'
 
 function LoginAndRegisterHandler() {
