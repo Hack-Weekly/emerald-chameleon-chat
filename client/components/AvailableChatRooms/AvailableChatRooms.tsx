32f0a5c743fcba5b6e5aab7b255f2f1c048a1d62
Jeni <ziahrahl@gmail.com>
2023-03-24 21:07:48 -0400

    style component

--- client/components/AvailableChatRooms/AvailableChatRooms.tsx
+++ client/components/AvailableChatRooms/AvailableChatRooms.tsx
@@ -1,27 +1,44 @@
 import React from 'react'
 import styles from './AvailableChatRooms.module.scss'
 import chatRooms from './mockData.json'
+import Link from 'next/link'
 
 function AvailableChatRooms() {
  
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h2>Available Chat Rooms</h2>
-      <div>This will display the availabe rooms</div>
-      <ChatRoomList />
+      <div className={styles.listWrapper}>
+        <ChatRoomList />
+        <Link href="/createChat" className={styles.createChatLink}>
+          Create Chat Room
+        </Link>
+      </div>
+      <Link href="/" className={styles.homeLink}>
+        Home
+      </Link>
     </div>
   )
 }
 
 function ChatRoomList() {
+  // const url = `${process.env.NEXT_PUBLIC_API_URL}/chat-room-endpoint`
+  // fetch list of chat rooms
+  // set chatRooms = data.json()
+
   return (
-    <div>
-      <ul>
-        {chatRooms.map((room, index) => (
-          <li key={index}>{room.name}</li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {chatRooms.map((room, index) => (
+        <li key={index}>
+          <Link href={`/chat-room/${room.id}`}>
+            <div className={styles.roomInfo}>
+              <h3>{room.name}</h3>
+              <p>{room.description}</p>
+            </div>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
