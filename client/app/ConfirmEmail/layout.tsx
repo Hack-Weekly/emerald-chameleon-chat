b57d1678fa8ffc77b1f8a5abcd966fe1648ecfbe
Jeni <ziahrahl@gmail.com>
2023-03-25 13:25:37 -0400

    remove email conf feature from this branch

--- client/app/ConfirmEmail/layout.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './EmailConfirmation.module.scss'
-
-interface Props {
-  children: React.ReactNode
-}
-
-export default function Layout({ children }: Props) {
-  return <div>{children}</div>
-}
