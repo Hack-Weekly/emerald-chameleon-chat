9903ee2d6243c6a6c37d53c387bf1b251fb2b3bc
Jeni <ziahrahl@gmail.com>
2023-03-26 13:10:51 -0400

    move component to chat-room page

--- client/app/PageOneExample/page.tsx
+++ client/app/PageOneExample/page.tsx
@@ -1,5 +1,3 @@
-import AvailableChatRooms from '@components/AvailableChatRooms/AvailableChatRooms'
-
 const PageOneExample = () => {
   const h1Text =
     "This is a server rendered page, because it doesn't have 'use client' this file"
@@ -7,8 +5,6 @@ const PageOneExample = () => {
     <div>
       <h1 className="h1">This is the /PageOneExample route</h1>
       <h2>{h1Text}</h2>
-      <hr />
-      <AvailableChatRooms />
     </div>
   )
 }
